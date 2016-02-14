d651e64b9c0259cc7947679b409d0681b80c836d
Kenneth Ortiz <kenortiznj@gmail.com>
2016-02-14 04:31:54 +0000

    Added validations in model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,4 @@
 class Quiz < ActiveRecord::Base
+  validates :name, present: true, length: { maximum: 50, minimum: 3 }
+  validates :answer, present: true, length: { maximum: 500, minimum: 10 }
 end
