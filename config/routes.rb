32e33a9bddbae2113f2ef8ab76efa6a41b34496a
Kenneth Ortiz <kenortiznj@gmail.com>
2016-02-14 00:21:38 +0000

    Installed rails 12factor gem. Changed config.assests.compile to true.Generated controller called quiz. Routed quiz as root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FirehoseQuiz::Application.routes.draw do
+  root 'quiz#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
