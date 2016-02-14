36cad2438682cd06ac44741846618c6f32e1d7b7
Kenneth Ortiz <kenortiznj@gmail.com>
2016-02-14 22:30:02 +0000

    Moved modal ato application.html.erb. Added answer box to index.html.erb.

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,7 +1,21 @@
 class QuizzesController < ApplicationController
 
   def index
+    @quiz = Quiz.all
+  end
 
+  def create
+    @quiz = Quiz.create(quiz_params)
+    if @quiz.invalid?
+      flash[:error] = '<strong>Could not save.</strong> The data you enteredd is invalid.'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:name, :answer)
   end
 
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,4 @@
 class Quiz < ActiveRecord::Base
-  validates :name, present: true, length: { maximum: 50, minimum: 3 }
-  validates :answer, present: true, length: { maximum: 500, minimum: 10 }
+  validates :name, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :answer, presence: true, length: { maximum: 500, minimum: 10 }
 end
